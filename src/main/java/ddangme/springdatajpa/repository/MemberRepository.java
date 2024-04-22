585ffc91322811b431037d0f41b78db7aea0ac94
ddangme <ssilver121@naver.com>
2024-04-23 02:54:15 +0900

    docs: @EntityGraph

--- src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
+++ src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
@@ -4,11 +4,13 @@ import ddangme.springdatajpa.dto.MemberDTO;
 import ddangme.springdatajpa.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.NamedEntityGraph;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -36,4 +38,25 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying
     @Query("UPDATE Member m SET m.age = m.age + 1 WHERE m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.team")
+    List<Member> findMemberFetchJoin();
+
+    // 공통 메서드 오버라이드
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    // JPQL + 엔티티 그래프
+    @EntityGraph(attributePaths = {"Team"})
+    @Query("SELECT m FROM Member m")
+    List<Member> findMemberEntityGraph();
+
+    // 메서드 이름으로 쿼리에서 특히 편리하다.
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findUsername(String username);
+
+    @EntityGraph("Member.all")
+    @Query("SELECT m FROM Member m")
+    List<Member> findMemberEntityGraph2();
 }
