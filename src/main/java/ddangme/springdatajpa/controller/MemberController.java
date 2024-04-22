6d9f777c0e8a7f5eb5ab71bdea1f575f457f3ae7
ddangme <ssilver121@naver.com>
2024-04-23 04:37:28 +0900

    docs: Web 확장 - 도메인 클래스 컨버터

---
+++ src/main/java/ddangme/springdatajpa/controller/MemberController.java
@@ -0,0 +1,20 @@
+package ddangme.springdatajpa.controller;
+
+import ddangme.springdatajpa.entity.Member;
+import ddangme.springdatajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+}
