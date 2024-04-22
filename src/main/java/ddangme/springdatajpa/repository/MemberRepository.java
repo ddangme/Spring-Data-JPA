9450e4522d7170dc95d41ce34b203bec46d16992
ddangme <ssilver121@naver.com>
2024-04-23 01:00:53 +0900

    docs: 메소드 이름으로 쿼리 생성

--- src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
+++ src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
@@ -3,4 +3,8 @@ package ddangme.springdatajpa.repository;
 import ddangme.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> { }
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+}
