ff359db2e6d08827e0bc16a5fd9c1c0480982ed9
ddangme <ssilver121@naver.com>
2024-04-23 01:12:05 +0900

    docs: @Query, 레포지토리 메소드에 쿼리 정의하기

--- src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
+++ src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package ddangme.springdatajpa.repository;
 
 import ddangme.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -10,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("SELECT m FROM Member m WHERE m.username= :username AND m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
