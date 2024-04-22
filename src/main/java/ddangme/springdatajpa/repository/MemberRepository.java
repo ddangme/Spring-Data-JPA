cbd4ee713a1ae55f49b9ce605df6cb577c17fb84
ddangme <ssilver121@naver.com>
2024-04-23 01:07:48 +0900

    docs: JPA NamedQuery

--- src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
+++ src/main/java/ddangme/springdatajpa/repository/MemberRepository.java
@@ -2,9 +2,12 @@ package ddangme.springdatajpa.repository;
 
 import ddangme.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findByUsername(@Param("username") String username);
 }
